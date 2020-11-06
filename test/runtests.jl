55cdae7b138f110659ffaec4c89d495d92de75b0
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2020-11-07 01:41:53 +0900

    use precise value in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ test_show(x) = show(IOBuffer(), x)
         df_sleep.Extra[df_sleep.Group .== "2"]
     )
     test_show(res_ttest)
-    @test isapprox(res_ttest["statistic"], 1.8608)
-    @test isapprox(res_ttest["df"], 17.776)
-    @test isapprox(res_ttest["P-value"], 0.079394)
+    @test isapprox(res_ttest["statistic"], 1.86081346748685)
+    @test isapprox(res_ttest["df"], 17.7764735161785)
+    @test isapprox(res_ttest["P-value"], 0.0793941401873582)
 end
